6d421a06e19ae56ba38e452e88d9dce99033f116
George <gabolaev98@gmail.com>
2018-02-06 02:52:51 +0300

    reduce -> Math.min/max.apply and minor fixes

--- source/minmax.js
+++ source/minmax.js
@@ -1,16 +1,9 @@
 'use strict';
 
-function getMin(data) {
-  return data.reduce((min, current) => current < min ? current : min, data[0]);
-}
-
-function getMax(data) {
-  return data.reduce((max, current) => current > max ? current : max, data[0]);
-}
-
 const HORRIBLE_REGEX = /^([-+]?)(Infinity|(((\.)?([0-9]+))|(([0-9]+)(\.)))(\e|\e\-)?([0-9]+)?)$/
+
 const minmax = inputLine => {
-    let parsedValues = inputLine.split(' ').filter(element => element.match(HORRIBLE_REGEX));
-    let floatedValues = parsedValues.map(value => parseFloat(value));
-    return ((!parsedValues.length) ? [undefined, undefined] : [getMin(floatedValues), getMax(floatedValues)]);
+    const parsedValues = inputLine.split(' ').filter(element => element.match(HORRIBLE_REGEX));
+    return ((!parsedValues.length) ? [undefined, undefined] : [Math.min.apply(null, parsedValues),
+                                                               Math.max.apply(null, parsedValues)]);
 };
